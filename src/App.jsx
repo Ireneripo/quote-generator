6eb57003a3a58f03a729f2eb241459c6f0031df2
Irene Riportella <riportellairene@gmail.com>
2025-05-07 15:59:40 +0200

    feat:✨ display quote container

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 
-function App() {
+export default function App() {
+  const [quotes, setQuotes] = React.useState([]);
+  const [quote, setQuote] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data);
+      });
+  }, []);
+
+  function getRandomQuote() {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(randomQuote);
+  }
+
   return (
     <>
       <h1 className="text-center text-4xl my-8 font-bold">
         Project 3: Quote Generator
       </h1>
+      <section className="text-center bg-cyan-100 max-w-3xl h-80 m-auto shadow-2xl rounded-md p-10">
+        <button
+          className="bg-blue-500 px-3 py-2 m-10 rounded-md cursor-pointer text-white font-bold"
+          onClick={getRandomQuote}
+        >
+          New quote
+        </button>
+        {quote && <p className="text-2xl">{`"${quote.q}"`}</p>}
+
+        {quote && (
+          <p className="text-xl font-bold italic">{`- ${quote.a} -`}</p>
+        )}
+      </section>
     </>
   );
 }
-
-export default App;
