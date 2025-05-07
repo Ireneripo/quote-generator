7d76f1edfc5a13efeb050d14d576c7986f1da1b0
Irene Riportella <riportellairene@gmail.com>
2025-05-07 09:57:32 +0200

    feat:🎉 first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl my-8 font-bold">
+        Project 3: Quote Generator
+      </h1>
+    </>
+  );
+}
+
+export default App;
